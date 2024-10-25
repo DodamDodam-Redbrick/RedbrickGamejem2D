eadd6308122ffa65085f1ce6578eac189ab299a2
cnh0728 <cnh0728@gmail.com>
2024-10-25 15:00:28 +0900

    Essential

---
+++ Assets/Scripts/Player/Data.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum EntityIndex
+{
+    //아군 엔티티
+    tank = 0,
+    sword = 1,
+    bow = 2,
+    caster = 3,
+    healer = 4,
+
+    //적 엔티티
+    slime = 101,
+    wolf = 102,
+}
+
+public class Entity
+{
+    public Entity(string path, double hp, double damage, double def, double moveSpeed, int weight)
+    {
+        this.prefabPath = path;
+        this.hp = hp;
+        this.damage = damage;
+        this.def = def;
+        this.moveSpeed = moveSpeed;
+        this.weight = weight;
+    }
+
+    public string prefabPath;
+    //프리팹 소환할 때 기본 수치들 적용시켜주기
+    public double hp;
+    public double damage;
+    public double def;
+    public double moveSpeed;
+    public int weight; //저지 가능 수 (적은 보통 1)
+}
+
+public static class Data
+{
+    public static Dictionary<EntityIndex, Entity> EntityData;
+
+
+}
---
+++ Assets/Scripts/Player/Player.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public static Player Instance;
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(this);
+
+        Instance = this;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
