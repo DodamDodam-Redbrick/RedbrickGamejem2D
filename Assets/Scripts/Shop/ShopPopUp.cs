c4b541bb498a6fe1c6db302bc47decf56356a35d
Hyunjun0701 <imissjuly2@gmail.com>
2024-11-02 00:26:39 +0900

    shoppanel 구현 (1단계

--- Assets/Scripts/Shop/ShopPopUp.cs
+++ Assets/Scripts/Shop/ShopPopUp.cs
@@ -1,18 +1,44 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ShopPopUp : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField]
+    Image rewardImage;
+
+    [SerializeField]
+    TextMeshProUGUI rewardDescription;
+
+    Reward reward;
+
+    public void Set(Reward reward)
     {
-        
+        this.reward = reward;
+
+        rewardImage.sprite = reward.thumbnail;
+        rewardDescription.text = reward.description;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OnClickCard()
     {
-        
+        //1. 데이터에 자기 리워드 추가시켜주고
+        switch (reward.rewardType)
+        {
+            case RewardType.gold:
+                Player.Instance.ChangeGold(reward.gold);
+                break;
+            case RewardType.unit_sword:
+                Player.Instance.AddUnit(reward.unit);
+                break;
+        }
+
+        //2. 부모가 되는 리워드 레이아웃 숨김
+        GameSystem.Instance.FinishGetReward();
+
+        //3. 다음 스텝 진행
+        //go to minimap
     }
 }
