31684d908f93385b6a4eb0871365c20579d7e81a
Hyunjun0701 <imissjuly2@gmail.com>
2024-11-03 15:55:25 +0900

    main화면 ui 완성 gamesystem 수정, datamanager 딕셔너리추가

--- Assets/Scripts/GameSystem.cs
+++ Assets/Scripts/GameSystem.cs
@@ -336,13 +336,11 @@ public class GameSystem : MonoBehaviour
 
     public void GetRandomReward(bool isOnlyUnit = false, UnityAction endAction = null)
     {
-        //랜덤으로 리워드 정하기
+        // 랜덤으로 리워드 정하기
         List<Reward> rewards = new List<Reward>();
-
-        while(rewards.Count <= rewardAmount)
+        while (rewards.Count < rewardAmount)
         {
-            RewardType rewardType = GetRandomEnumType<RewardType>();
-
+            RewardType rewardType = GetRandomRewardType();
             Reward reward = GetReward(rewardType, isOnlyUnit);
 
             if (reward == null)
@@ -352,6 +350,36 @@ public class GameSystem : MonoBehaviour
         }
 
         rewardPanel.ShowPopupPanel(rewards, endAction);
+    }
+
+    private RewardType GetRandomRewardType()
+    {
+        float totalWeight = DataManager.Instance.rewardChanceDatas
+                            .Where(pair => !IsShopReward(pair.Key)) // shop_potion 타입 제외
+                            .Sum(pair => pair.Value);
+        float randomValue = UnityEngine.Random.Range(0, totalWeight);
+        float cumulativeWeight = 0f;
+
+        foreach (var rewardChance in DataManager.Instance.rewardChanceDatas)
+        {
+            if (IsShopReward(rewardChance.Key))
+                continue;
+
+            cumulativeWeight += rewardChance.Value;
+            if (randomValue <= cumulativeWeight)
+            {
+                return rewardChance.Key;
+            }
+        }
+
+        return RewardType.reward_gold; // Default return value, should not reach here
+    }
+
+    private bool IsShopReward(RewardType rewardType)
+    {
+        return rewardType == RewardType.shop_potion_one
+            || rewardType == RewardType.shop_potion_two
+            || rewardType == RewardType.shop_potion_three;
     }
 
     public void FinishGetReward(UnityAction endAction = null)
